ee78b3ae420f4ecc879ef087bc8e142e159b60f7
Arturo Vaine <arturo.vaine@gmail.com>
2022-03-21 12:05:05 -0300

    fix: req1 e 2, mudança para fluxo assincrono

--- index.js
+++ index.js
@@ -20,14 +20,33 @@ app.listen(PORT, () => {
 // O endpoint deve retornar um array com todas as pessoas palestrantes cadastradas, devendo retornar o status 200.
 // Caso não exista nenhuma pessoa palestrante cadastrada o endpoint deve retornar um array vazio e o status 200.
 
-const fs = require('fs');
+const fs = require('fs').promises;
 
-const talkersFile = fs.readFileSync('./talker.json', 'utf-8');
+// const readData = () => fs.readFileSync(
+// ler arquivo
+// converter para json 
+// parse
 
-app.get('/talker', (req, res) => {
-  // console.log(JSON.parse(talkersFile));
-  // console.log(JSON.parse(talkersFile).length);
+app.get('/talker', async (req, res) => {
+  const talkers = await fs.readFile('./talker.json', 'utf8');
 
-  if (talkersFile.length !== 0) { return (res.status(200).send(JSON.parse(talkersFile))); }
-  return res.status(200).send(JSON.parse('[]'));
+  return (res.status(200).json(JSON.parse(talkers)));
+});
+
+// 2 - Crie o endpoint GET /talker/:id
+// O endpoint deve retornar uma pessoa palestrante com base no id da rota. Devendo retornar o status 200 ao fazer uma requisição /talker/1,
+// com o seguinte corpo: ;...Henrique
+
+// Caso não seja encontrada uma pessoa palestrante com base no id da rota, o endpoint deve retornar o status 404 com o seguinte corpo:
+// { "message": "Pessoa palestrante não encontrada" }
+
+app.get('/talker/:id', async (req, res) => {
+  const talkers = await fs.readFile('./talker.json', 'utf-8');
+  
+  const { id } = req.params;
+
+  const talkerById = JSON.parse(talkers).find((talker) => talker.id === parseInt(id, 10));
+
+  if (!talkerById) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+  return res.status(200).json(talkerById);
 });
